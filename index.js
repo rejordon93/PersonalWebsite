7b7e226eeb7f1008e06b8b03c37952a0372b12da
Ethan Jordon <72542756+rejordon93@users.noreply.github.com>
2022-03-19 01:11:36 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.querySelector(".link1").onclick = function (event) {
+  let url = "https://github.com/rejordon93/PersonalWebsite";
+  window.location.href = url;
+};
+document.querySelector(".link2").onclick = function (event) {
+  let url = "https://github.com/rejordon93/myFirstWebsite";
+  window.location.href = url;
+};
+document.querySelector(".link3").onclick = function (event) {
+  let url = "https://github.com/rejordon93/DiceGame";
+  window.location.href = url;
+};
